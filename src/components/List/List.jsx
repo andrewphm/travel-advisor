82bab67d3e3921877cfbd73398431bc06f86001b
andrewphm <andrpham@gmail.com>
2021-09-22 16:07:16 -0400

    Add a form and menu for user input on the category they'd like to see

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Material UI
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
@@ -9,13 +9,21 @@ import useStyles from './styles';
 
 const List = () => {
     const classes = useStyles();
-
+    const [type, setType] = useState('restaurants');
 
     return (
         <div className={classes.container}>
             <Typography variant="h4">
                 Restaurants, Hotels & Attractions around you
             </Typography>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Type</InputLabel>
+                <Select value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
 
         </div>
     );
